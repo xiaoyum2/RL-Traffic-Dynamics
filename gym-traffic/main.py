774c6c180e11aecc79f744da00ac0a523fad0e90
xiaoyum2 <61123854+xiaoyum2@users.noreply.github.com>
2022-01-30 19:32:52 -0600

    Add files via upload

--- gym-traffic/main.py
+++ gym-traffic/main.py
@@ -5,7 +5,7 @@ from models.policy import random_policy
 from options import parse_options
 import logging as log
 
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, A2C
 from stable_baselines3.common.env_checker import check_env
 
 
@@ -22,19 +22,21 @@ if __name__ == "__main__":
     env = TrafficEnv()
     obs = env.reset()
 
-    check_env(env)
+    # check_env(env)
+    # print("Checked!")
    
     # action = random_policy(obs['observation'], obs['desired_goal'],env)
     # obs, reward, done, info = env.step(action)
-    model = PPO("MultiInputPolicy", env, verbose=1)
-    model.learn(total_timesteps=20000)
-    model.save("PPO_first_trial")
+    # model = PPO("MultiInputPolicy", env, verbose=1)
+    model = PPO("MlpPolicy", env, verbose=1, learning_rate=1)
+    model.learn(total_timesteps=1)
     print("Trained!")
     
     for i in range(1000):
         action, _states = model.predict(obs, deterministic=True)
         obs, reward, done, info = env.step(action)
-        env.render()
+        # env.render()
+        print(env.state)
         if done:
             obs = env.reset()
     env.close()
@@ -43,6 +45,6 @@ if __name__ == "__main__":
     # If we want, we can substitute a goal here and re-compute
     # the reward. For instance, we can just pretend that the desired
     # goal was what we achieved all along.
-    substitute_goal = obs['achieved_goal'].copy()
-    substitute_reward = env.compute_reward( obs['achieved_goal'], substitute_goal, info)
-    print('reward is {}, substitute_reward is {}'.format(reward, substitute_reward))
+    # substitute_goal = obs['achieved_goal'].copy()
+    # substitute_reward = env.compute_reward( obs['achieved_goal'], substitute_goal, info)
+    # print('reward is {}, substitute_reward is {}'.format(reward, substitute_reward))
