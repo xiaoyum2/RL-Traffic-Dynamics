85e8413f00aa06b478e2f658cb1e2028b119408b
xiaoyum2 <61123854+xiaoyum2@users.noreply.github.com>
2022-02-08 15:40:47 -0600

    Add files via upload

--- gym-traffic/main.py
+++ gym-traffic/main.py
@@ -27,8 +27,8 @@ if __name__ == "__main__":
    
     # action = random_policy(obs['observation'], obs['desired_goal'],env)
     # obs, reward, done, info = env.step(action)
-    # model = PPO("MultiInputPolicy", env, verbose=1)
-    model = DDPG("MlpPolicy", env, verbose=1, learning_rate=0.1)
+    model = PPO("MlpPolicy", env, verbose=1, learning_rate=1)
+    # model = DDPG("MlpPolicy", env, verbose=1, learning_rate=0.5)
 
     action, _states = model.predict(obs, deterministic=True)
 
@@ -42,16 +42,21 @@ if __name__ == "__main__":
     print("Trained!")
     
     obs = env.reset()
-    for i in range(1000):
-        if i%10==0:
-            print("Road status before:", obs)
-            print("Action:", action)
+    for i in range(20):
+        print("Road status before:", obs)
+        print("Action:", action)
+        # if i%10==0:
+        #     print("Road status before:", obs)
+        #     print("Action:", action)
         action, _states = model.predict(obs, deterministic=True)
         obs, reward, done, info = env.step(action)
         # env.render()
-        if i%10==0:
-            print("Road status new:", obs)
-            print("Reward:", reward)
+        print("Road status new:", obs)
+        print("Reward:", reward)
+        print("\n")
+        # if i%10==0:
+        #     print("Road status new:", obs)
+        #     print("Reward:", reward)
         if done:
             obs = env.reset()
     env.close()
