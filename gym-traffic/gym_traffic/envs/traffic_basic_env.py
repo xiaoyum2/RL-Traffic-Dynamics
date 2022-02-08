8589794f96e5e947302800d71fe5e56302e63b42
xiaoyum2 <61123854+xiaoyum2@users.noreply.github.com>
2022-02-08 15:41:20 -0600

    Add files via upload

--- gym-traffic/gym_traffic/envs/traffic_basic_env.py
+++ gym-traffic/gym_traffic/envs/traffic_basic_env.py
@@ -9,27 +9,42 @@ class TrafficEnv(gym.Env):
 
     def __init__(self):
         super(TrafficEnv, self).__init__()
-        # action_space: auto_headway on each link
-        self.action_space = spaces.Box(low=np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0]), high=np.array([+10.0, +10.0, +10.0, +10.0, +10.0, +10.0]), dtype=np.float32)
-        # obs_space: number of vehicle on each link
-        # self.observation_space = spaces.Box(low=np.array([0, 0, 0, 0, 0, 0]), high=np.array([1000, 1200, 600, 600, 960, 840]), dtype=np.float32)
-        self.observation_space = spaces.Box(low=np.array([0, 0, 0, 0, 0, 0]), high=np.array([2000, 2000, 2000, 2000, 2000, 2000]), dtype=np.float32)
+        # # action_space: auto_headway on each link
+        # self.action_space = spaces.Box(low=np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0]), high=np.array([+10.0, +10.0, +10.0, +10.0, +10.0, +10.0]), dtype=np.float32)
+        # # obs_space: number of vehicle on each link
+        # # self.observation_space = spaces.Box(low=np.array([0, 0, 0, 0, 0, 0]), high=np.array([1000, 1200, 600, 600, 960, 840]), dtype=np.float32)
+        # self.observation_space = spaces.Box(low=np.array([0, 0, 0, 0, 0, 0]), high=np.array([2000, 2000, 2000, 2000, 2000, 2000]), dtype=np.float32)
         
-
-        self.num_link = 6
+        # self.num_link = 6
         
-        self.total_veh_num = 1100
-        self.state = np.array([900,200,100,100,900,200])
-        self.lanes_link = np.array([4, 4, 2, 2, 2, 2])
-        self.length_link = np.array([500, 600, 500, 500, 800, 700])
-        self.free_v_link = np.array([40, 50, 40, 40, 20, 40])
-        self.alpha_link = np.array([0.4, 0.4, 0.2, 0.2, 0.2, 0.4])
-        self.jam_density_link = np.array([2.0, 2.0, 1.2, 1.2, 1.2, 1.2])
-        self.human_headway_link = np.array([3.0, 3.0, 3.0, 3.0, 3.0, 3.0])
+        # self.total_veh_num = 1100
+        # self.state = np.array([900,200,100,100,900,200])
+        # self.lanes_link = np.array([4, 4, 2, 2, 2, 2])
+        # self.length_link = np.array([500, 600, 500, 500, 800, 700])
+        # self.free_v_link = np.array([40, 50, 40, 40, 20, 40])
+        # self.alpha_link = np.array([0.4, 0.4, 0.2, 0.2, 0.2, 0.4])
+        # self.jam_density_link = np.array([2.0, 2.0, 1.2, 1.2, 1.2, 1.2])
+        # self.human_headway_link = np.array([3.0, 3.0, 3.0, 3.0, 3.0, 3.0])
+
+        #simplest network with 4 links and 2 nodes (1 OD pair)
+        self.action_space = spaces.Box(low=np.array([2.5, 1.0, 1.0, 1.0, 2.5, 1.0, 1.0, 1.0]), high=np.array([+10.0, +10.0, +10.0, +10.0, +10.0, +10.0, +10.0, +10.0]), dtype=np.float32)
+        self.observation_space = spaces.Box(low=np.array([0, 0, 0, 0, 0, 0, 0, 0]), high=np.array([2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000]), dtype=np.float32)
+
+        self.num_link = 8
+        
+        self.total_veh_num = 630
+        self.state = np.array([600,10,10,10,600,10,10,10])
+        self.lanes_link = np.array([2, 2, 2, 2, 2, 2, 2, 2])
+        self.length_link = np.array([500, 600, 500, 500, 500, 600, 500, 500])
+        self.free_v_link = np.array([50, 50, 50, 50, 50, 50, 50, 50])
+        self.alpha_link = np.array([0.2, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.4])
+        self.jam_density_link = np.array([2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0])
+        self.human_headway_link = np.array([3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0])
 
 
-        #dynamic coef.
-        self.miu = 0.001
+
+        #dynamic coef, which needed to be twitched from 1
+        self.miu = 0.1
 
     def step(self, action):
         flag_done = False
@@ -44,6 +59,7 @@ class TrafficEnv(gym.Env):
         flow_link = np.zeros(self.num_link)
         latency_link = np.zeros(self.num_link)
 
+        # old definition of flow and latency
         for i in range(self.num_link):
             if(density_link[i]<cri_density_link[i]):
                 flow_link[i] = self.free_v_link[i]*density_link[i]
@@ -60,49 +76,106 @@ class TrafficEnv(gym.Env):
                 flow_link[i] = self.free_v_link[i]*cri_density_link[i]*(self.jam_density_link[i]-density_link[i])/(self.jam_density_link[i]-cri_density_link[i])
                 latency_link[i] = self.length_link[i]*(self.jam_density_link[i]/flow_link[i]+(cri_density_link[i]-self.jam_density_link[i])/(self.free_v_link[i]*cri_density_link[i]))
 
-        # calculate reward
+        # new definition of flow and latency
+        # for i in range(self.num_link):
+        #     if(density_link[i]<cri_density_link[i]):
+        #         flow_link[i] = self.free_v_link[i]*density_link[i]
+
+        #     elif(density_link[i]>self.jam_density_link[i]):
+        #         flow_link[i] = 0
+
+        #     else:
+        #         flow_link[i] = self.free_v_link[i]*cri_density_link[i]*(self.jam_density_link[i]-density_link[i])/(self.jam_density_link[i]-cri_density_link[i])
+            
+        #     latency_link[i] = flow_link[i]*density_link[i]
+    
+
         reward = 0
         for i in range(self.num_link):
             reward -= self.state[i]*latency_link[i]
         # print("reward:", reward)
 
+
         # calculate vehcle number on each link for next time step
         # need future modification for generalizing with dif  ferent path number!
         
         # for this simple case, we have four paths: {0,4},{0,2,5},{1,5},{1,3,4}
+        # if(flag_done==False):
+        #     state_new = np.zeros(self.num_link, dtype=np.float32)
+        #     path_1_num = self.state[0]-self.state[2]
+        #     path_2_num = self.state[2]
+        #     path_3_num = self.state[1]-self.state[3]
+        #     path_4_num = self.state[3]
+
+        #     path_1_val = path_1_num*np.exp(-self.miu*(latency_link[0]+latency_link[4]))
+        #     path_2_val = path_2_num*np.exp(-self.miu*(latency_link[0]+latency_link[2]+latency_link[5]))
+        #     path_3_val = path_3_num*np.exp(-self.miu*(latency_link[1]+latency_link[5]))
+        #     path_4_val = path_4_num*np.exp(-self.miu*(latency_link[1]+latency_link[3]+latency_link[4]))
+        #     total_val = path_1_val+path_2_val+path_3_val+path_4_val
+        #     # print(self.state)
+        #     # print("total divide:", total_val)
+        #     # print("Latencies:", latency_link)
+        #     # print("Path_Val:", np.array([path_1_val, path_2_val, path_3_val, path_4_val]))
+
+        #     _path_1 =  self.total_veh_num*path_1_val/(total_val)
+        #     _path_2 =  self.total_veh_num*path_2_val/(total_val)
+        #     _path_3 =  self.total_veh_num*path_3_val/(total_val)
+        #     _path_4 =  self.total_veh_num*path_4_val/(total_val)
+        #     state_new[0] = _path_1+_path_2
+        #     state_new[1] = _path_3+_path_4
+        #     state_new[2] = _path_2
+        #     state_new[3] = _path_4
+        #     state_new[4] = _path_1+_path_4
+        #     state_new[5] = _path_2+_path_3
+
+        #     if(np.sum(np.abs(self.state-state_new))<10):
+        #         flag_done = True
+            
+        #     self.state = state_new
+
+
+        # below is the dynamics of case with 4 paths of same OD pair
         if(flag_done==False):
             state_new = np.zeros(self.num_link, dtype=np.float32)
-            path_1_num = self.state[0]-self.state[2]
-            path_2_num = self.state[2]
-            path_3_num = self.state[1]-self.state[3]
+            path_1_num = self.state[0]
+            path_2_num = self.state[1]
+            path_3_num = self.state[2]
             path_4_num = self.state[3]
 
             path_1_val = path_1_num*np.exp(-self.miu*(latency_link[0]+latency_link[4]))
-            path_2_val = path_2_num*np.exp(-self.miu*(latency_link[0]+latency_link[2]+latency_link[5]))
-            path_3_val = path_3_num*np.exp(-self.miu*(latency_link[1]+latency_link[5]))
-            path_4_val = path_4_num*np.exp(-self.miu*(latency_link[1]+latency_link[3]+latency_link[4]))
+            path_2_val = path_2_num*np.exp(-self.miu*(latency_link[1]+latency_link[5]))
+            path_3_val = path_3_num*np.exp(-self.miu*(latency_link[2]+latency_link[6]))
+            path_4_val = path_4_num*np.exp(-self.miu*(latency_link[3]+latency_link[7]))
             total_val = path_1_val+path_2_val+path_3_val+path_4_val
-            # print(self.state)
+
+            # print("State:",self.state)
+            # print("Action:", action)
             # print("total divide:", total_val)
             # print("Latencies:", latency_link)
             # print("Path_Val:", np.array([path_1_val, path_2_val, path_3_val, path_4_val]))
+            # print("\n")
 
             _path_1 =  self.total_veh_num*path_1_val/(total_val)
             _path_2 =  self.total_veh_num*path_2_val/(total_val)
             _path_3 =  self.total_veh_num*path_3_val/(total_val)
             _path_4 =  self.total_veh_num*path_4_val/(total_val)
-            state_new[0] = _path_1+_path_2
-            state_new[1] = _path_3+_path_4
-            state_new[2] = _path_2
+            state_new[0] = _path_1
+            state_new[1] = _path_2
+            state_new[2] = _path_3
             state_new[3] = _path_4
-            state_new[4] = _path_1+_path_4
-            state_new[5] = _path_2+_path_3
+            state_new[4] = _path_1
+            state_new[5] = _path_2
+            state_new[6] = _path_3
+            state_new[7] = _path_4
+
 
             if(np.sum(np.abs(self.state-state_new))<10):
                 flag_done = True
             
             self.state = state_new
 
+
+        
         # print("State:",self.state) 
         if (flag_done):
             done = True
@@ -114,7 +187,13 @@ class TrafficEnv(gym.Env):
         return self.state, reward, done, info
 
     def reset(self):
-        self.state = np.array([900,200,100,100,900,200])
+        # reset state to starting case
+
+        # for 6 links occasion
+        # self.state = np.array([900,200,100,100,900,200])
+
+        # for 4 paths with same OD pair
+        self.state = np.array([600,10,10,10,600,10,10,10])
         return self.state
   
     def render(self, mode='human'):
